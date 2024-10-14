9572619907f2ca7ad2b716e347f8957f438a3700
ThanhLongNg <23520885@gm.uit.edu.vn>
2024-10-14 20:44:21 +0700

    Add files via upload

---
+++ Lab1.1_23520885_Nguyễn Thành Long.sql
@@ -0,0 +1,301 @@
+-- BAI TAP 1.2
+
+CREATE DATABASE QLGV
+USE QLGV
+
+--- KHOA ---
+CREATE TABLE KHOA
+(
+	MAKHOA	char(4),
+	TENKHOA	varchar(40),
+	NGTLAP	smalldatetime,
+	TRGKHOA	char(4),
+	constraint PK_KHOA primary key(MAKHOA)
+)
+
+--- MONHOC ---
+CREATE TABLE MONHOC
+(
+	MAMH	char(10),
+	TENMH	varchar(40),
+	TCLT	tinyint,
+	TCTH	tinyint,
+	MAKHOA	char(4),
+	constraint PK_MH primary key(MAMH)
+)
+
+--- DIEUKIEN ---
+CREATE TABLE DIEUKIEN
+(
+	MAMH		char(10),
+	MAMH_TRUOC	char(10),
+	constraint PK_DK primary key(MAMH,MAMH_TRUOC)
+)
+
+--- GIAOVIEN ---
+CREATE TABLE GIAOVIEN
+(
+	MAGV		char(4),
+	HOTEN		varchar(40),
+	HOCVI		varchar(10),
+	HOCHAM		varchar(10),
+	GIOITINH	varchar(3),
+	NGSINH		smalldatetime,
+	NGVL		smalldatetime,
+	HESO		numeric(4,2),
+	MUCLUONG	money,
+	MAKHOA		char(4),
+	constraint PK_GV primary key(MAGV)
+)
+
+--- LOP ---
+CREATE TABLE LOP
+(
+	MALOP	char(3),
+	TENLOP	varchar(40),
+	TRGLOP	char(5),
+	SISO	tinyint,
+	MAGVCN	char(4),
+	constraint PK_LOP primary key(MALOP)
+)
+
+--- HOCVIEN ---
+CREATE TABLE HOCVIEN
+(
+	MAHV		char(5),
+	HO		varchar(40),
+	TEN		varchar(10),
+	NGSINH		smalldatetime,
+	GIOITINH	varchar(3),
+	NOISINH		varchar(40),
+	MALOP		char(3),
+	constraint PK_HV primary key(MAHV)
+)
+
+--- GIANGDAY ---
+CREATE TABLE GIANGDAY
+(
+	MALOP	char(3),
+	MAMH	char(10),
+	MAGV	char(4),
+	HOCKY	tinyint,
+	NAM	smallint,
+	TUNGAY	smalldatetime,
+	DENNGAY	smalldatetime,
+	constraint PK_GD primary key(MALOP,MAMH)
+)
+
+--- KETQUATHI ---
+CREATE TABLE KETQUATHI
+(
+	MAHV	char(5),
+	MAMH	char(10),
+	LANTHI	tinyint,
+	NGTHI	smalldatetime,
+	DIEM	numeric(4,2),
+	KQUA	varchar(10),
+	constraint PK_KQ primary key(MAHV,MAMH,LANTHI)
+)
+
+
+-- KHOA 	
+ALTER TABLE KHOA ADD CONSTRAINT FK_KHOA FOREIGN KEY(TRGKHOA) REFERENCES GIAOVIEN(MAGV)
+-- MONHOC 		
+ALTER TABLE MONHOC ADD CONSTRAINT FK_MH FOREIGN KEY(MAKHOA) REFERENCES KHOA(MAKHOA)
+-- DIEUKIEN 	
+ALTER TABLE DIEUKIEN ADD CONSTRAINT FK01_DK FOREIGN KEY(MAMH) REFERENCES MONHOC(MAMH)
+ALTER TABLE DIEUKIEN ADD CONSTRAINT FK02_DK FOREIGN KEY(MAMH_TRUOC) REFERENCES MONHOC(MAMH)
+-- GIAOVIEN
+ALTER TABLE GIAOVIEN ADD CONSTRAINT FK_GV FOREIGN KEY(MAKHOA) REFERENCES KHOA(MAKHOA)
+-- LOP
+ALTER TABLE LOP ADD CONSTRAINT FK01_LOP FOREIGN KEY(TRGLOP) REFERENCES HOCVIEN(MAHV)
+ALTER TABLE LOP ADD CONSTRAINT FK02_LOP FOREIGN KEY(MAGVCN) REFERENCES GIAOVIEN(MAGV)
+-- HOCVIEN
+ALTER TABLE HOCVIEN ADD CONSTRAINT FK_HV FOREIGN KEY(MALOP) REFERENCES LOP(MALOP)
+-- GIANGDAY
+ALTER TABLE GIANGDAY ADD CONSTRAINT FK01_GD FOREIGN KEY(MALOP) REFERENCES LOP(MALOP)
+ALTER TABLE GIANGDAY ADD CONSTRAINT FK02_GD FOREIGN KEY(MAMH) REFERENCES MONHOC(MAMH)
+ALTER TABLE GIANGDAY ADD CONSTRAINT FK03_GD FOREIGN KEY(MAGV) REFERENCES GIAOVIEN(MAGV)
+-- KETQUATHI
+ALTER TABLE KETQUATHI ADD CONSTRAINT FK01_KQ FOREIGN KEY(MAHV) REFERENCES HOCVIEN(MAHV)
+ALTER TABLE KETQUATHI ADD CONSTRAINT FK02_KQ FOREIGN KEY(MAMH) REFERENCES MONHOC(MAMH)
+
+set dateformat dmy
+
+-- NHAPKHOA
+insert into KHOA values('KHMT','Khoa hoc may tinh','06/07/2005','GV01')
+insert into KHOA values('HTTT','He thong thong tin','06/07/2005','GV02')
+insert into KHOA values('CNPM','Cong nghe phan mem','06/07/2005','GV04')
+insert into KHOA values('MTT','Mang va truyen thong','20/10/2005','GV03')
+insert into KHOA values('KTMT','Ky thuat may tinh','20/12/2005', Null)
+
+-- NHAPLOP
+insert into LOP values('K11','Lop 1 khoa 1','K1108',11,'GV07')
+insert into LOP values('K12','Lop 2 khoa 1','K1205',12,'GV09')
+insert into LOP values('K13','Lop 3 khoa 1','K1305',12,'GV14')
+
+-- NHAPMONHOC
+insert into MONHOC values('THDC','Tin hoc dai cuong',4,1,'KHMT')
+insert into MONHOC values('CTRR','Cau truc roi rac',5,0,'KHMT')
+insert into MONHOC values('CSDL','Co so du lieu',3,1,'HTTT')
+insert into MONHOC values('CTDLGT','Cau truc du lieu va giai thuat',3,1,'KHMT')
+insert into MONHOC values('PTTKTT','Phan tich thiet ke thuat toan',3,0,'KHMT')
+insert into MONHOC values('DHMT','Do hoa may tinh',3,1,'KHMT')
+insert into MONHOC values('KTMT','Kien truc may tinh',3,0,'KTMT')
+insert into MONHOC values('TKCSDL','Thiet ke co so du lieu',3,1,'HTTT')
+insert into MONHOC values('PTTKHTTT','Phan tich thiet ke he thong thong tin',4,1,'HTTT')
+insert into MONHOC values('HDH','He dieu hanh',4,0,'KTMT')
+insert into MONHOC values('NMCNPM','Nhap mon cong nghe phan mem',3,0,'CNPM')
+insert into MONHOC values('LTCFW','Lap trinh C for win',3,1,'CNPM')
+insert into MONHOC values('LTHDT','Lap trinh huong doi tuong',3,1,'CNPM')
+
+-- NHAPDIEUKIEN
+insert into DIEUKIEN values('CSDL','CTRR')
+insert into DIEUKIEN values('CSDL','CTDLGT')
+insert into DIEUKIEN values('CTDLGT','THDC')
+insert into DIEUKIEN values('PTTKTT','THDC')
+insert into DIEUKIEN values('PTTKTT','CTDLGT')
+insert into DIEUKIEN values('DHMT','THDC')
+insert into DIEUKIEN values('LTHDT','THDC')
+insert into DIEUKIEN values('PTTKHTTT','CSDL')
+
+
+-- NHAPGIAOVIEN
+insert into GIAOVIEN values('GV01','Ho Thanh Son','PTS','GS','Nam','05/02/1950','01/11/2004',5,2250000,'KHMT');
+insert into GIAOVIEN values('GV02','Tran Tam Thanh','TS','PGS','Nam','17/12/1965','20/4/2004',4.5,2025000,'HTTT');
+insert into GIAOVIEN values('GV03','Do Nghiem Phung','TS','GS','Nu','08/01/1950','23/9/2004',4,1800000,'CNPM');
+insert into GIAOVIEN values('GV04','Tran Nam Son','TS','PGS','Nam','22/2/1961','01/12/2005',4.5,2025000,'KTMT');
+insert into GIAOVIEN values('GV05','Mai Thanh Danh','ThS','GV','Nam','03/12/1958','01/12/2005',3,1350000,'HTTT');
+insert into GIAOVIEN values('GV06','Tran Doan Hung','TS','GV','Nam','03/11/1953','01/12/2005',4.5,2025000,'KHMT');
+insert into GIAOVIEN values('GV07','Nguyen Minh Tien','ThS','GV','Nam','23/11/1971','03/01/2005',4,1800000,'KHMT');
+insert into GIAOVIEN values('GV08','Le Thi Tran','KS','Null','Nu','26/3/1974','03/01/2005',1.69,760500,'KHMT');
+insert into GIAOVIEN values('GV09','Nguyen To Lan','ThS','GV','Nu','31/12/1966','03/01/2005',4,1800000,'HTTT');
+insert into GIAOVIEN values('GV10','Le Tran Anh Loan','KS','Null','Nu','17/7/1972','03/01/2005',1.86,837000,'CNPM');
+insert into GIAOVIEN values('GV11','Ho Thanh Tung','CN','GV','Nam','01/12/1980','15/5/2005',2.67,1201500,'MTT');
+insert into GIAOVIEN values('GV12','Tran Van Anh','CN','Null','Nu','29/3/1981','15/5/2005',1.69,760500,'CNPM');
+insert into GIAOVIEN values('GV13','Nguyen Linh Dan','CN','Null','Nu','23/5/1980','15/5/2005',1.69,760500,'KTMT');
+insert into GIAOVIEN values('GV14','Truong Minh Chau','ThS','GV','Nu','30/11/1976','15/5/2005',3,1350000,'MTT');
+insert into GIAOVIEN values('GV15','Le Ha Thanh','ThS','GV','Nam','05/04/1978','15/5/2005',3,1350000,'KHMT');
+
+
+-- NHAPGIANGDAY
+insert into GIANGDAY values('K12','THDC','GV06',1,2006,'01/02/2006','05/12/2006');
+insert into GIANGDAY values('K13','THDC','GV15',1,2006,'01/02/2006','05/12/2006');
+insert into GIANGDAY values('K11','CTRR','GV02',1,2006,'09/01/2006','17/5/2006');
+insert into GIANGDAY values('K12','CTRR','GV02',1,2006,'09/01/2006','17/5/2006');
+insert into GIANGDAY values('K13','CTRR','GV08',1,2006,'09/01/2006','17/5/2006');
+insert into GIANGDAY values('K11','CSDL','GV05',2,2006,'06/01/2006','15/7/2006');
+insert into GIANGDAY values('K12','CSDL','GV09',2,2006,'06/01/2006','15/7/2006');
+insert into GIANGDAY values('K13','CTDLGT','GV15',2,2006,'06/01/2006','15/7/2006');
+insert into GIANGDAY values('K13','CSDL','GV05',3,2006,'08/01/2006','15/12/2006');
+insert into GIANGDAY values('K13','DHMT','GV07',3,2006,'08/01/2006','15/12/2006');
+insert into GIANGDAY values('K11','CTDLGT','GV15',3,2006,'08/01/2006','15/12/2006');
+insert into GIANGDAY values('K12','CTDLGT','GV15',3,2006,'08/01/2006','15/12/2006');
+insert into GIANGDAY values('K11','HDH','GV04',1,2007,'01/02/2007','18/2/2007');
+insert into GIANGDAY values('K12','HDH','GV04',1,2007,'01/02/2007','20/3/2007');
+insert into GIANGDAY values('K11','DHMT','GV07',1,2007,'18/2/2007','20/3/2007');
+
+-- NHAPKETQUATHI
+
+insert into KETQUATHI values('K1101','CSDL',1,'20/7/2006',10,'Dat');
+insert into KETQUATHI values('K1101','CTDLGT',1,'28/12/2006',9,'Dat');
+insert into KETQUATHI values('K1101','THDC',1,'20/5/2006',9,'Dat');
+insert into KETQUATHI values('K1101','CTRR',1,'13/5/2006',9.5,'Dat');
+insert into KETQUATHI values('K1102','CSDL',1,'20/7/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1102','CSDL',2,'27/7/2006',4.25,'Khong Dat');
+insert into KETQUATHI values('K1102','CSDL',3,'08/10/2006',4.5,'Khong Dat');
+insert into KETQUATHI values('K1102','CTDLGT',1,'28/12/2006',4.5,'Khong Dat');
+insert into KETQUATHI values('K1102','CTDLGT',2,'01/05/2007',4,'Khong Dat');
+insert into KETQUATHI values('K1102','CTDLGT',3,'15/1/2007',6,'Dat');
+insert into KETQUATHI values('K1102','THDC',1,'20/5/2006',5,'Dat');
+insert into KETQUATHI values('K1102','CTRR',1,'13/5/2006',7,'Dat');
+insert into KETQUATHI values('K1103','CSDL',1,'20/7/2006',3.5,'Khong Dat');
+insert into KETQUATHI values('K1103','CSDL',2,'27/7/2006',8.25,'Dat');
+insert into KETQUATHI values('K1103','CTDLGT',1,'28/12/2006',7,'Dat');
+insert into KETQUATHI values('K1103','THDC',1,'20/5/2006',8,'Dat');
+insert into KETQUATHI values('K1103','CTRR',1,'13/5/2006',6.5,'Dat');
+insert into KETQUATHI values('K1104','CSDL',1,'20/7/2006',3.75,'Khong Dat');
+insert into KETQUATHI values('K1104','CTDLGT',1,'28/12/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1104','THDC',1,'20/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1104','CTRR',1,'13/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1104','CTRR',2,'20/5/2006',3.5,'Khong Dat');
+insert into KETQUATHI values('K1104','CTRR',3,'30/6/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1201','CSDL',1,'20/7/2006',6,'Dat');
+insert into KETQUATHI values('K1201','CTDLGT',1,'28/12/2006',5,'Dat');
+insert into KETQUATHI values('K1201','THDC',1,'20/5/2006',8.5,'Dat');
+insert into KETQUATHI values('K1201','CTRR',1,'13/5/2006',9,'Dat');
+insert into KETQUATHI values('K1202','CSDL',1,'20/7/2006',8,'Dat');
+insert into KETQUATHI values('K1202','CTDLGT',1,'28/12/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1202','CTDLGT',2,'01/05/2007',5,'Dat');
+insert into KETQUATHI values('K1202','THDC',1,'20/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1202','THDC',2,'27/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1202','CTRR',1,'13/5/2006',3,'Khong Dat');
+insert into KETQUATHI values('K1202','CTRR',2,'20/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1202','CTRR',3,'30/6/2006',6.25,'Dat');
+insert into KETQUATHI values('K1203','CSDL',1,'20/7/2006',9.25,'Dat');
+insert into KETQUATHI values('K1203','CTDLGT',1,'28/12/2006',9.5,'Dat');
+insert into KETQUATHI values('K1203','THDC',1,'20/5/2006',10,'Dat');
+insert into KETQUATHI values('K1203','CTRR',1,'13/5/2006',10,'Dat');
+insert into KETQUATHI values('K1204','CSDL',1,'20/7/2006',8.5,'Dat');
+insert into KETQUATHI values('K1204','CTDLGT',1,'28/12/2006',6.75,'Dat');
+insert into KETQUATHI values('K1204','THDC',1,'20/5/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1204','CTRR',1,'13/5/2006',6,'Dat');
+insert into KETQUATHI values('K1301','CSDL',1,'20/12/2006',4.25,'Khong Dat');
+insert into KETQUATHI values('K1301','CTDLGT',1,'25/7/2006',8,'Dat');
+insert into KETQUATHI values('K1301','THDC',1,'20/5/2006',7.75,'Dat');
+insert into KETQUATHI values('K1301','CTRR',1,'13/5/2006',8,'Dat');
+insert into KETQUATHI values('K1302','CSDL',1,'20/12/2006',6.75,'Dat');
+insert into KETQUATHI values('K1302','CTDLGT',1,'25/7/2006',5,'Dat');
+insert into KETQUATHI values('K1302','THDC',1,'20/5/2006',8,'Dat');
+insert into KETQUATHI values('K1302','CTRR',1,'13/5/2006',8.5,'Dat');
+insert into KETQUATHI values('K1303','CSDL',1,'20/12/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1303','CTDLGT',1,'25/7/2006',4.5,'Khong Dat');
+insert into KETQUATHI values('K1303','CTDLGT',2,'08/07/2006',4,'Khong Dat');
+insert into KETQUATHI values('K1303','CTDLGT',3,'15/8/2006',4.25,'Khong Dat');
+insert into KETQUATHI values('K1303','THDC',1,'20/5/2006',4.5,'Khong Dat');
+insert into KETQUATHI values('K1303','CTRR',1,'13/5/2006',3.25,'Khong Dat');
+insert into KETQUATHI values('K1303','CTRR',2,'20/5/2006',5,'Dat');
+insert into KETQUATHI values('K1304','CSDL',1,'20/12/2006',7.75,'Dat');
+insert into KETQUATHI values('K1304','CTDLGT',1,'25/7/2006',9.75,'Dat');
+insert into KETQUATHI values('K1304','THDC',1,'20/5/2006',5.5,'Dat');
+insert into KETQUATHI values('K1304','CTRR',1,'13/5/2006',5,'Dat');
+insert into KETQUATHI values('K1305','CSDL',1,'20/12/2006',9.25,'Dat');
+insert into KETQUATHI values('K1305','CTDLGT',1,'25/7/2006',10,'Dat');
+insert into KETQUATHI values('K1305','THDC',1,'20/5/2006',8,'Dat');
+insert into KETQUATHI values('K1305','CTRR',1,'13/5/2006',10,'Dat');
+
+
+-- NHAPHOCVIEN
+insert into HOCVIEN values('K1101','Nguyen Van','A','27/1/1986','Nam','TpHCM','K11');
+insert into HOCVIEN values('K1102','Tran Ngoc','Han','14/3/1986','Nu','Kien Giang','K11');
+insert into HOCVIEN values('K1103','Ha Duy','Lap','18/4/1986','Nam','Nghe An','K11');
+insert into HOCVIEN values('K1104','Tran Ngoc','Linh','30/3/1986','Nu','Tay Ninh','K11');
+insert into HOCVIEN values('K1105','Tran Minh','Long','27/2/1986','Nam','TpHCM','K11');
+insert into HOCVIEN values('K1106','Le Nhat','Minh','24/1/1986','Nam','TpHCM','K11');
+insert into HOCVIEN values('K1107','Nguyen Nhu','Nhut','27/1/1986','Nam','Ha Noi','K11');
+insert into HOCVIEN values('K1108','Nguyen Manh','Tam','27/2/1986','Nam','Kien Giang','K11');
+insert into HOCVIEN values('K1109','Phan Thi Thanh','Tam','27/1/1986','Nu','Vinh Long','K11');
+insert into HOCVIEN values('K1110','Le Hoai','Thuong','02/05/1986','Nu','Can Tho','K11');
+insert into HOCVIEN values('K1111','Le Ha','Vinh','25/12/1986','Nam','Vinh Long','K11');
+insert into HOCVIEN values('K1201','Nguyen Van','B','02/11/1986','Nam','TpHCM','K12');
+insert into HOCVIEN values('K1202','Nguyen Thi Kim','Duyen','18/1/1986','Nu','TpHCM','K12');
+insert into HOCVIEN values('K1203','Tran Thi Kim','Duyen','17/9/1986','Nu','TpHCM','K12');
+insert into HOCVIEN values('K1204','Truong My','Hanh','19/5/1986','Nu','Dong Nai','K12');
+insert into HOCVIEN values('K1205','Nguyen Thanh','Nam','17/4/1986','Nam','TpHCM','K12');
+insert into HOCVIEN values('K1206','Nguyen Thi Truc','Thanh','03/04/1986','Nu','Kien Giang','K12');
+insert into HOCVIEN values('K1207','Tran Thi Bich','Thuy','02/08/1986','Nu','Nghe An','K12');
+insert into HOCVIEN values('K1208','Huynh Thi Kim','Trieu','04/08/1986','Nu','Tay Ninh','K12');
+insert into HOCVIEN values('K1209','Pham Thanh','Trieu','23/2/1986','Nam','TpHCM','K12');
+insert into HOCVIEN values('K1210','Ngo Thanh','Tuan','14/2/1986','Nam','TpHCM','K12');
+insert into HOCVIEN values('K1211','Do Thi','Xuan','03/09/1986','Nu','Ha Noi','K12');
+
+
+-----------------------------------------------
+SELECT * FROM HOCVIEN
+SELECT * FROM LOP
+SELECT * FROM KHOA
+SELECT * FROM MONHOC
+SELECT * FROM DIEUKIEN
+SELECT * FROM GIAOVIEN
+SELECT * FROM GIANGDAY
+SELECT * FROM KETQUATHI
